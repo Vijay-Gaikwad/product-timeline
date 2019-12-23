1d1c16a4640dddd9046334dc0c9eeaed2a5a5364
vijay-gaikwad <jaideep.bajaj@synechron.com>
2019-12-23 18:58:52 +0530

    Timeline product display functionality added.

--- src/components/timeline/products/products.js
+++ src/components/timeline/products/products.js
@@ -1,62 +1,77 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './products.css';
 import productImage from "../../../assets/images/placeholder-product.png"; // Tell Webpack this JS file uses this image
 import { ButtonToolbar, Button, InputGroup, FormControl } from 'react-bootstrap';
+import productsConstant from "../../../constants/constant.json"
+import _ from 'lodash';
 
 
-function Products(props) {
-  return (
-    <div className="product-container">
-      <div className="box">
-        <img className="product-image" src={productImage} alt="Product"></img>
-        <ButtonToolbar>
-          <Button className="plus-button" variant="outline-primary" size="sm">+</Button>
-          <Button className="minus-button" variant="outline-primary" size="sm">-</Button>
-        </ButtonToolbar>
-        <InputGroup className="product-quantity mb-3">
-          <FormControl
-            placeholder="Qty"
-          />
-        </InputGroup>
+class Products extends Component {
+  constructor(props) {
+    super(props);
+    this.handleButton = this.handleButton.bind(this);
+    this.state = {
+      product: [],
+      button: {
+        plus: 0,
+        minus: 0
+      },
+      quantity: 0
+    }
+  }
+
+  findProduct(year) {
+    return [_.find(productsConstant.products, { 'year': year })];
+  }
+
+  handleButton(e) {
+    const products = localStorage.getItem("productsStorage");
+    console.log("products", JSON.parse(products));
+    if (e.target.name === 'plus') {
+      this.setState({
+        quantity
+          : this.state.quantity + 1
+      })
+    } else {
+      if (e.target.name === 'minus' || this.state.quantity !== 0)
+        this.setState({
+          quantity
+            : this.state.quantity - 1
+        })
+    }
+  }
+
+  componentDidMount() {
+    this.setState({ product: this.findProduct(this.props.year) });
+  }
+
+  render() {
+    return (
+      <div className="product-container">
+        {(this.state.product[0] && !_.isUndefined(this.state.product[0].products) ) && (
+          this.state.product[0].products.map((item, id) => {
+            return (<div className="box" key={id}>
+              <img className="product-image" src={productImage} alt="Product"></img>
+              <h6>{item.title}</h6>
+              <ButtonToolbar>
+                <Button className="plus-button" name="plus" variant="outline-primary" size="sm" onClick={this.handleButton}>+</Button>
+                <Button className="minus-button" variant="outline-primary" size="sm" onClick={this.handleButton}>-</Button>
+              </ButtonToolbar>
+              <InputGroup className="product-quantity mb-3">
+                <FormControl
+                  placeholder="Qty"
+                  value={this.state.quantity}
+                  disabled
+                />
+              </InputGroup>
+            </div>
+            )
+          })
+        )
+        }
       </div>
-      <div className="box">
-        <img className="product-image" src={productImage} alt="Product"></img>
-        <ButtonToolbar>
-          <Button className="plus-button" variant="outline-primary" size="sm">+</Button>
-          <Button className="minus-button" variant="outline-primary" size="sm">-</Button>
-        </ButtonToolbar>
-        <InputGroup className="product-quantity mb-3">
-          <FormControl
-            placeholder="Qty"
-          />
-        </InputGroup>
-      </div>
-      <div className="box">
-        <img className="product-image" src={productImage} alt="Product"></img>
-        <ButtonToolbar>
-          <Button className="plus-button" variant="outline-primary" size="sm">+</Button>
-          <Button className="minus-button" variant="outline-primary" size="sm">-</Button>
-        </ButtonToolbar>
-        <InputGroup className="product-quantity mb-3">
-          <FormControl
-            placeholder="Qty"
-          />
-        </InputGroup>
-      </div>
-      <div className="box">
-        <img className="product-image" src={productImage} alt="Product"></img>
-        <ButtonToolbar>
-          <Button className="plus-button" variant="outline-primary" size="sm">+</Button>
-          <Button className="minus-button" variant="outline-primary" size="sm">-</Button>
-        </ButtonToolbar>
-        <InputGroup className="product-quantity mb-3">
-          <FormControl
-            placeholder="Qty"
-          />
-        </InputGroup>
-      </div>
-    </div>
-  )
+    );
+  }
 }
 
 export default Products;
--- src/components/timeline/timeline.jsx
+++ src/components/timeline/timeline.jsx
@@ -1,28 +1,35 @@
 import React, { Component } from 'react';
 import './timeline.css';
 import Products from './products/products';
+import _ from 'lodash';
+import productsConstant from "../../constants/constant.json"
 
 class Timeline extends Component {
     constructor(props) {
         super(props);
         this.state = {}
     }
+
+    componentDidMount() {
+        localStorage.setItem("productsStorage", JSON.stringify(productsConstant));
+    }
+
     render() {
         const totalYears = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
         return totalYears.map((key, value) => {
             return (
                 <div key={value}>
-                    {(key%2 !== 0) ? (
+                    {(key % 2 !== 0) ? (
                         <div className="timeline-container left">
                             <div className="content">
                                 <h2>{`200${key}`}</h2>
-                                <Products></Products>
+                                <Products year = {_.parseInt(`200${key}`)}></Products>
                             </div>
                         </div>
                     ) : (<div className="timeline-container right">
                         <div className="content">
-                            <h2>{(key !== 0)?(`200${key}`):(`201${key}`)}</h2>
-                            <Products></Products>
+                            <h2>{(key !== 0) ? (`200${key}`) : (`201${key}`)}</h2>
+                            <Products year = {_.parseInt((key !== 0) ? (`200${key}`) : (`201${key}`))}></Products>
                         </div>
                     </div>
                         )}
