302ba03250bac18862b014bbdf0ec44c959d09ff
vijay-gaikwad <jaideep.bajaj@synechron.com>
2019-12-23 20:30:35 +0530

    Product component changed to functional component with hooks.

--- src/components/timeline/products/products.js
+++ src/components/timeline/products/products.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './products.css';
 import productImage from "../../../assets/images/placeholder-product.png"; // Tell Webpack this JS file uses this image
 import { ButtonToolbar, Button, InputGroup, FormControl } from 'react-bootstrap';
@@ -6,71 +6,53 @@ import productsConstant from "../../../constants/constant.json"
 import _ from 'lodash';
 
 
-class Products extends Component {
-  constructor(props) {
-    super(props);
-    this.handleButton = this.handleButton.bind(this);
-    this.state = {
-      product: [],
-      button: {
-        plus: 0,
-        minus: 0
-      },
-      quantity: 0
+function Products(props) {
+  const [product, setProduct] = useState([]);
+  const [quantity, setQuantity] = useState(0);
+
+  
+  useEffect(() => {
+    const products = findProduct(props.year);
+    setProduct(products);
+  }, [props])
+
+  const handleButton = (e, quantity, setQuantity) => {
+    if (e.target.name === 'plus') {
+      setQuantity(quantity+1);
+    } else {
+      if (e.target.name === 'minus' || quantity !== 0)
+        setQuantity(quantity-1);
     }
   }
-
-  findProduct(year) {
+  
+  const findProduct = (year) => {
     return [_.find(productsConstant.products, { 'year': year })];
   }
 
-  handleButton(e) {
-    localStorage.getItem("productsStorage");
-    if (e.target.name === 'plus') {
-      this.setState({
-        quantity
-          : this.state.quantity + 1
-      })
-    } else {
-      if (e.target.name === 'minus' || this.state.quantity !== 0)
-        this.setState({
-          quantity
-            : this.state.quantity - 1
+  return (
+    <div className="product-container">
+      {(product[0] && !_.isUndefined(product[0].products)) && (
+        product[0].products.map((item, id) => {
+          return (<div className="box" key={id}>
+            <img className="product-image" src={productImage} alt="Product"></img>
+            <h6>{item.title}</h6>
+            <ButtonToolbar>
+              <Button className="plus-button" name="plus" variant="outline-primary" size="sm" onClick={(e) => handleButton(e,quantity, setQuantity)}>+</Button>
+              <Button className="minus-button" variant="outline-primary" size="sm" onClick={(e) => handleButton(e,quantity,setQuantity)}>-</Button>
+            </ButtonToolbar>
+            <InputGroup className="product-quantity mb-3">
+              <FormControl
+                value={quantity}
+                disabled
+              />
+            </InputGroup>
+          </div>
+          )
         })
-    }
-  }
-
-  componentDidMount() {
-    this.setState({ product: this.findProduct(this.props.year) });
-  }
-
-  render() {
-    return (
-      <div className="product-container">
-        {(this.state.product[0] && !_.isUndefined(this.state.product[0].products) ) && (
-          this.state.product[0].products.map((item, id) => {
-            return (<div className="box" key={id}>
-              <img className="product-image" src={productImage} alt="Product"></img>
-              <h6>{item.title}</h6>
-              <ButtonToolbar>
-                <Button className="plus-button" name="plus" variant="outline-primary" size="sm" onClick={this.handleButton}>+</Button>
-                <Button className="minus-button" variant="outline-primary" size="sm" onClick={this.handleButton}>-</Button>
-              </ButtonToolbar>
-              <InputGroup className="product-quantity mb-3">
-                <FormControl
-                  placeholder="Qty"
-                  value={this.state.quantity}
-                  disabled
-                />
-              </InputGroup>
-            </div>
-            )
-          })
-        )
-        }
-      </div>
-    );
-  }
+      )
+      }
+    </div>
+  )
 }
 
 export default Products;
